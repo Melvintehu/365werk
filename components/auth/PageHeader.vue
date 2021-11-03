5885cecba55e8835b5834fb64c95c6a4abfe18b3
Melvin <melvindrachten@hotmail.com>
2021-11-03 11:40:47 +0100

    added an page header component for the auth pages

---
+++ components/auth/PageHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="page-header">
+    <div class="page-header__title-wrapper">
+      <img src="~/assets/img/blobs/svg/blob-page-title.svg">
+      <h1 class="page-header__title">
+        <slot />
+      </h1>
+    </div>
+    <h2 class="page-header__sub-title">
+      <slot name="subTitle" />
+    </h2>
+  </header>
+</template>
+<script>
+export default {}
+</script>
