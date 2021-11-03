bcc61f5e57aa2c19f2e5f3e8a51e9c5551c190d5
Melvin <melvindrachten@hotmail.com>
2021-11-03 21:54:11 +0100

    added a getter for retreiving the authenticated user

--- store/auth.js
+++ store/auth.js
@@ -1,23 +1,32 @@
-export const state = () => ({})
+export const state = () => ({
+  user: null
+})
 
-export const mutations = {}
+export const mutations = {
+  setUser (state, user) {
+    state.user = user
+  }
+}
 
 export const actions = {
-  login (state, payload) {
+  login ({ commit }) {
     return new Promise((resolve, reject) => {
-      localStorage.setItem('user', JSON.stringify({
+      const user = {
         email: 'melvin.tehu@gmail.com',
         first_name: 'Melvin',
         last_name: 'Tehubijuluw'
-      }))
+      }
+      localStorage.setItem('user', JSON.stringify(user))
+      commit('setUser', user)
       setTimeout(() => {
         resolve()
       }, 1000)
     })
   },
-  logout (state, payload) {
+  logout ({ commit }) {
     return new Promise((resolve, reject) => {
       localStorage.removeItem('user')
+      commit('setUser', null)
       setTimeout(() => {
         resolve()
       }, 1000)
@@ -29,5 +38,8 @@ export const getters = {
   isAuthenticated: (state) => {
     const user = localStorage.getItem('user')
     return user !== null
+  },
+  getUser: (state) => {
+    return state.user
   }
 }
