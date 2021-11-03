9cf4f43af9a836b92c39d6eac7a7dc8668f6b4ba
Melvin <melvindrachten@hotmail.com>
2021-11-03 11:39:02 +0100

    initial commit

---
+++ components/NavAuth.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- Logo and navigation -->
+  <div class="nav">
+    <NuxtLink class="nav--link-default" to="/login">
+      365werk
+    </NuxtLink>
+
+    <NuxtLink class="nav--link-default" to="/register">
+      Register
+    </NuxtLink>
+
+    <div class="nav--link-group">
+      <NuxtLink class="nav--link-bordered" to="/login">
+        Log in to your account
+      </NuxtLink>
+      <img class="nav--link-group-blob" src="~/assets/img/blobs/svg/blob-log-in-to-account.svg">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
