5f72978427fd87dacee9e35a6b7a4bae7c2f2d88
Melvin <melvindrachten@hotmail.com>
2021-11-03 15:58:03 +0100

    added responsive account progress bar

--- pages/account.vue
+++ pages/account.vue
@@ -25,7 +25,22 @@
           </div>
         </div>
         <div class="page-top-bar__right">
-          <div class="account-progress"></div>
+          <div class="account-progress">
+            <div class="account-progress__labels-wrapper">
+              <p class="account-progress__label-left">
+                Account progress
+              </p>
+              <p class="account-progress__label-right">
+                Complete it for even better job oppurtunities.
+              </p>
+            </div>
+            <div class="account-progress__progress-bar">
+              <div class="account-progress__progress-bar-fill"></div>
+            </div>
+            <p class="account-progress__percentage">
+              30%
+            </p>
+          </div>
         </div>
       </div>
       <div class="page-inner">
