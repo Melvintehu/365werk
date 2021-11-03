58e651bd32c8e28145120e31181b519a560f1fc9
Melvin <melvindrachten@hotmail.com>
2021-11-03 11:41:08 +0100

    added a generic input component for reuse

---
+++ components/form/InputDefault.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="input">
+    <div class="input__label-wrapper">
+      <label class="input__label">
+        <slot name="label" />
+      </label>
+      <a :href="to" class="input__help">
+        <slot name="help" />
+      </a>
+    </div>
+    <input :name="name" :type="type" :placeholder="placeholder" class="input__default">
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+
+    type: {
+      type: String,
+      default: 'text'
+    },
+
+    placeholder: {
+      type: String,
+      default: 'Type here'
+    },
+
+    to: {
+      type: String,
+      default: '#'
+    }
+  }
+}
+</script>
