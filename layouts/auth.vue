2c3ead25f83fb05eb4fbbcb050b9f1815f59d219
Melvin <melvindrachten@hotmail.com>
2021-11-03 11:52:52 +0100

    added layout files for auth pages and for account pages

---
+++ layouts/auth.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nav-auth />
+    <Nuxt />
+  </div>
+</template>
+<script>
+import NavAuth from '../components/NavAuth'
+export default {
+  components: { NavAuth }
+}
+</script>
