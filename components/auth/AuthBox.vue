b25a1fed214f610e5c4df40c2879aba1bb173021
Melvin <melvindrachten@hotmail.com>
2021-11-03 11:40:12 +0100

    added a auth box wrapper component

---
+++ components/auth/AuthBox.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="auth-box">
+    <div class="auth-box__header">
+      <h3 class="auth-box__header-title">
+        <slot name="label" />
+      </h3>
+      <NuxtLink class="auth-box__header-link" :to="linksTo">
+        <span class="auth-box__header-link-label">
+          <slot name="linkLabel" />
+        </span>
+        <span class="auth-box__header-link-icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          ><rect fill="none" height="24" width="24" /><path
+            d="M15,5l-1.41,1.41L18.17,11H2V13h16.17l-4.59,4.59L15,19l7-7L15,5z"
+          /></svg>
+        </span>
+      </NuxtLink>
+    </div>
+    <slot />
+    <div class="auth-box__button-wrapper">
+      <slot name="button" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    linksTo: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
