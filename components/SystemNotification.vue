ed0fb61de272bc3779241255268e0494a6adc37e
Melvin <melvindrachten@hotmail.com>
2021-11-04 16:22:40 +0100

    added basic layout and styling for system notifications

---
+++ components/SystemNotification.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="system-notification">
+    <p class="system-notification__title" >Something went wrong...</p>
+    <p class="system-notification__description" >
+      Whoops we're afraid that feature doesn't exist yet.
+    </p>
+  </div>
+</template>
+<script>
+export default {}
+</script>
