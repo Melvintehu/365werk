1efba01057453dddd3434f2951101b2adefd1118
Melvin <melvindrachten@hotmail.com>
2021-11-04 16:04:02 +0100

    current language is now displayed

--- components/TheLanguageSwitch.vue
+++ components/TheLanguageSwitch.vue
@@ -1,6 +1,6 @@
 <template>
-  <div @click="expanded = !expanded" class="language-switch">
-    <img class="language-switch__flag" src="~/assets/img/country-flags/svg/nl.svg" alt="">
+  <div class="language-switch" @click="expanded = !expanded">
+    <img class="language-switch__flag" :src="require(`~/assets/img/country-flags/svg/${currentLanguage}.svg`)" alt="">
     <figure class="language-switch__icon">
       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>
     </figure>
@@ -27,11 +27,18 @@
   </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   data () {
     return {
       expanded: false
     }
+  },
+  computed: {
+    ...mapGetters({
+      currentLanguage: 'localization/getCurrentLanguage'
+    })
   },
   methods: {
     switchLanguage (language) {
