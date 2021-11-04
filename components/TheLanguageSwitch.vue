724900ddbe75ee92757e9dd090bcae33457aa24c
Melvin <melvindrachten@hotmail.com>
2021-11-04 12:26:05 +0100

    added basic layout and styling for language switch

---
+++ components/TheLanguageSwitch.vue
@@ -0,0 +1,11 @@
+<template>
+  <div @click="expand" class="language-switch">
+    <img class="language-switch__flag" src="~/assets/img/country-flags/svg/nl.svg" alt="">
+    <figure class="language-switch__icon">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>
+    </figure>
+  </div>
+</template>
+<script>
+export default {}
+</script>
