1965fe08e0e723775dddae6f0f46a7af31be5176
unknown <melvindrachten@hotmail.com>
2021-11-06 17:00:00 +0100

    added cursor pointer, added live validation to register page

--- pages/register.vue
+++ pages/register.vue
@@ -16,28 +16,52 @@
         <template slot="linkLabel">
           {{ translate('already_an_account') }}
         </template>
-
-        <input-default name="email" :placeholder="translate('placeholder_email')">
-          <template slot="label">
-            {{ translate('email_address') }}
-          </template>
-        </input-default>
-
-        <input-default name="phone" :placeholder="translate('placeholder_phone_number')">
-          <template slot="label">
-            {{ translate('phone_number') }}
-          </template>
-        </input-default>
-
-        <input-default target="_blank" to="https://www.lastpass.com/nl/features/password-generator" name="password" type="password" :placeholder="translate('enter_password')">
-          <template slot="label">
-            {{ translate('password') }}
-          </template>
-          <template slot="help">
-            {{ translate('generate_password') }}
-          </template>
-        </input-default>
-
+        <input-validator
+          sync-with="login"
+          :rules="[
+            { type: 'required', message: translate('required', translate('email_address')) },
+            { type: 'email', message: translate('invalid', translate('email_address')) },
+            { type: 'minLength', rule: 2, message: translate('min_length', 2) },
+            { type: 'maxLength', rule: 255, message: translate('max_length', 255) }
+          ]"
+          :message="translate('validation_errors', translate('email_address'))"
+        >
+          <input-default name="email" :placeholder="translate('placeholder_email')">
+            <template slot="label">
+              {{ translate('email_address') }}
+            </template>
+          </input-default>
+        </input-validator>
+        <input-validator
+          :rules="[
+              { type: 'required', message: translate('required', translate('phone_number')) },
+              { type: 'phoneNumber', message: translate('invalid', `${translate('phone_number')} (+31612345678)`) }
+            ]"
+          :message="translate('validation_errors', translate('phone_number'))"
+        >
+          <input-default name="phone" :placeholder="translate('placeholder_phone_number')">
+            <template slot="label">
+              {{ translate('phone_number') }}
+            </template>
+          </input-default>
+        </input-validator>
+        <input-validator
+          sync-with="login"
+          :rules="[
+            { type: 'required', message: translate('required', translate('password')) },
+            { type: 'minLength', rule: 12, message: translate('min_length', 12) },
+          ]"
+          :message="translate('validation_errors', translate('password'))"
+        >
+          <input-default target="_blank" to="https://www.lastpass.com/nl/features/password-generator" name="password" type="password" :placeholder="translate('enter_password')">
+            <template slot="label">
+              {{ translate('password') }}
+            </template>
+            <template slot="help">
+              {{ translate('generate_password') }}
+            </template>
+          </input-default>
+        </input-validator>
         <!--Login button-->
         <button slot="button" class="button__main button__main--hover">
           {{ translate('register_button') }}
@@ -51,9 +75,11 @@
 import AuthBox from '../components/auth/AuthBox'
 import PageHeader from '../components/auth/PageHeader'
 import InputDefault from '../components/form/InputDefault'
+import InputValidator from '../components/form/InputValidator'
 
 export default {
   components: {
+    InputValidator,
     InputDefault,
     PageHeader,
     AuthBox
