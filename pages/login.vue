5e67020a5e645d13b16b133e5df5756092496831
Melvin <melvindrachten@hotmail.com>
2021-11-07 08:44:31 +0100

    added waves to the bottom of the page

--- pages/login.vue
+++ pages/login.vue
@@ -1,59 +1,66 @@
 <template>
-  <div class="auth-page">
-    <div class="container">
-      <page-header>
-        {{ translate('login_page_title') }}
-        <template slot="subTitle">
-          {{ translate('login_page_sub_title') }}
-        </template>
-      </page-header>
+  <div>
+    <div class="auth-page">
+      <div class="container">
+        <page-header>
+          {{ translate('login_page_title') }}
+          <template slot="subTitle">
+            {{ translate('login_page_sub_title') }}
+          </template>
+        </page-header>
 
-      <auth-box links-to="/register">
-        <template slot="label">
-          {{ translate('login') }}
-        </template>
-        <template slot="linkLabel">
-          {{ translate('no_account') }}
-        </template>
-        <input-validator
-          sync-with="login"
-          :rules="[
+        <auth-box links-to="/register">
+          <template slot="label">
+            {{ translate('login') }}
+          </template>
+          <template slot="linkLabel">
+            {{ translate('no_account') }}
+          </template>
+          <input-validator
+            sync-with="login"
+            :rules="[
             { type: 'required', message: translate('required', translate('email_address')) },
             { type: 'email', message: translate('invalid', translate('email_address')) },
             { type: 'minLength', rule: 2, message: translate('min_length', 2) },
             { type: 'maxLength', rule: 255, message: translate('max_length', 255) }
           ]"
-          :message="translate('validation_errors', translate('email_address'))"
-        >
-          <input-default v-model="email" name="email" :placeholder="translate('enter_email')">
-            <template slot="label">
-              {{ translate('email_address') }}
-            </template>
-          </input-default>
-        </input-validator>
-        <input-validator
-          sync-with="login"
-          :rules="[
+            :message="translate('validation_errors', translate('email_address'))"
+          >
+            <input-default v-model="email" name="email" :placeholder="translate('enter_email')">
+              <template slot="label">
+                {{ translate('email_address') }}
+              </template>
+            </input-default>
+          </input-validator>
+          <input-validator
+            sync-with="login"
+            :rules="[
             { type: 'required', message: translate('required', translate('password')) },
             { type: 'minLength', rule: 12, message: translate('min_length', 12) },
           ]"
-          :message="translate('validation_errors', translate('password'))"
-        >
-          <input-default v-model="password" to="/forgot-password" name="password" type="password" :placeholder="translate('enter_password')">
-            <template slot="label">
-              {{ translate('password') }}
-            </template>
-            <template slot="help">
-              {{ translate('forgot_password') }}
-            </template>
-          </input-default>
-        </input-validator>
-        <disable-on-error slot="button" sync-with="login">
-          <button  class="button__main button__main--hover" @click="login()">
-            {{ translate('login_button') }}
-          </button>
-        </disable-on-error>
-      </auth-box>
+            :message="translate('validation_errors', translate('password'))"
+          >
+            <input-default v-model="password" to="/forgot-password" name="password" type="password" :placeholder="translate('enter_password')">
+              <template slot="label">
+                {{ translate('password') }}
+              </template>
+              <template slot="help">
+                {{ translate('forgot_password') }}
+              </template>
+            </input-default>
+          </input-validator>
+          <disable-on-error slot="button" sync-with="login">
+            <button  class="button__main button__main--hover" @click="login()">
+              {{ translate('login_button') }}
+            </button>
+          </disable-on-error>
+        </auth-box>
+      </div>
+    </div>
+    <div class="page-waves">
+      <figure class="page-waves__wrapper">
+        <img src="~/assets/img/waves/svg/waves.svg">
+      </figure>
     </div>
   </div>
 </template>
--- pages/register.vue
+++ pages/register.vue
@@ -1,72 +1,79 @@
 <template>
-  <div class="auth-page">
-    <div class="container">
-      <!-- Page title, subtitle -->
-      <page-header>
-        {{ translate('register_page_title') }}
-        <template slot="subTitle">
-          {{ translate('register_page_sub_title') }}
-        </template>
-      </page-header>
+  <div>
+    <div class="auth-page">
+      <div class="container">
+        <!-- Page title, subtitle -->
+        <page-header>
+          {{ translate('register_page_title') }}
+          <template slot="subTitle">
+            {{ translate('register_page_sub_title') }}
+          </template>
+        </page-header>
 
-      <auth-box links-to="/login">
-        <template slot="label">
-          {{ translate('register') }}
-        </template>
-        <template slot="linkLabel">
-          {{ translate('already_an_account') }}
-        </template>
-        <input-validator
-          sync-with="login"
-          :rules="[
+        <auth-box links-to="/login">
+          <template slot="label">
+            {{ translate('register') }}
+          </template>
+          <template slot="linkLabel">
+            {{ translate('already_an_account') }}
+          </template>
+          <input-validator
+            sync-with="login"
+            :rules="[
             { type: 'required', message: translate('required', translate('email_address')) },
             { type: 'email', message: translate('invalid', translate('email_address')) },
             { type: 'minLength', rule: 2, message: translate('min_length', 2) },
             { type: 'maxLength', rule: 255, message: translate('max_length', 255) }
           ]"
-          :message="translate('validation_errors', translate('email_address'))"
-        >
-          <input-default name="email" :placeholder="translate('placeholder_email')">
-            <template slot="label">
-              {{ translate('email_address') }}
-            </template>
-          </input-default>
-        </input-validator>
-        <input-validator
-          :rules="[
+            :message="translate('validation_errors', translate('email_address'))"
+          >
+            <input-default name="email" :placeholder="translate('placeholder_email')">
+              <template slot="label">
+                {{ translate('email_address') }}
+              </template>
+            </input-default>
+          </input-validator>
+          <input-validator
+            :rules="[
               { type: 'required', message: translate('required', translate('phone_number')) },
               { type: 'phoneNumber', message: translate('invalid', `${translate('phone_number')} (+31612345678)`) }
             ]"
-          :message="translate('validation_errors', translate('phone_number'))"
-        >
-          <input-default name="phone" :placeholder="translate('placeholder_phone_number')">
-            <template slot="label">
-              {{ translate('phone_number') }}
-            </template>
-          </input-default>
-        </input-validator>
-        <input-validator
-          sync-with="login"
-          :rules="[
+            :message="translate('validation_errors', translate('phone_number'))"
+          >
+            <input-default name="phone" :placeholder="translate('placeholder_phone_number')">
+              <template slot="label">
+                {{ translate('phone_number') }}
+              </template>
+            </input-default>
+          </input-validator>
+          <input-validator
+            sync-with="login"
+            :rules="[
             { type: 'required', message: translate('required', translate('password')) },
             { type: 'minLength', rule: 12, message: translate('min_length', 12) },
           ]"
-          :message="translate('validation_errors', translate('password'))"
-        >
-          <input-default target="_blank" to="https://www.lastpass.com/nl/features/password-generator" name="password" type="password" :placeholder="translate('enter_password')">
-            <template slot="label">
-              {{ translate('password') }}
-            </template>
-            <template slot="help">
-              {{ translate('generate_password') }}
-            </template>
-          </input-default>
-        </input-validator>
-        <!--Login button-->
-        <button slot="button" class="button__main button__main--hover">
-          {{ translate('register_button') }}
-        </button>
-      </auth-box>
+            :message="translate('validation_errors', translate('password'))"
+          >
+            <input-default target="_blank" to="https://www.lastpass.com/nl/features/password-generator" name="password" type="password" :placeholder="translate('enter_password')">
+              <template slot="label">
+                {{ translate('password') }}
+              </template>
+              <template slot="help">
+                {{ translate('generate_password') }}
+              </template>
+            </input-default>
+          </input-validator>
+          <!--Login button-->
+          <button slot="button" class="button__main button__main--hover">
+            {{ translate('register_button') }}
+          </button>
+        </auth-box>
+      </div>
+    </div>
+    <div class="page-waves">
+      <figure class="page-waves__wrapper">
+        <img src="~/assets/img/waves/svg/waves.svg">
+      </figure>
     </div>
   </div>
 </template>
