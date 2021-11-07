78861f6a80dcb413dc6cd2fa018c7f04f6ccdef3
Melvin <melvindrachten@hotmail.com>
2021-11-07 08:54:32 +0100

    language switch closes when user clicks outside it

--- components/TheLanguageSwitch.vue
+++ components/TheLanguageSwitch.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="language-switch" @click="expanded = !expanded">
+  <div v-click-outside="close" class="language-switch" @click="expanded = !expanded">
     <img class="language-switch__flag" :src="require(`~/assets/img/country-flags/svg/${currentLanguage}.svg`)" alt="">
     <figure class="language-switch__icon">
       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>
@@ -43,6 +43,9 @@ export default {
   methods: {
     switchLanguage (language) {
       this.$store.commit('localization/setLanguage', language)
+    },
+    close () {
+      this.expanded = false
     }
   }
 }
